35e489a49ceb1c1007cfbf6040c7e216cde0a534
Luke Shay <shay.luke17@gmail.com>
2020-03-06 07:51:48 -0600

    Updated build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,6 +19,13 @@ rm -rf restapi.log
 #    -x check \
 #    || exit 1
 
-GRADLE_OPTS="-Xmx64m -Dorg.gradle.jvmargs='-Xmx256m -XX:MaxPermSize=64m'" ./gradlew clean build
+GRADLE_OPTS="-Xmx64m -Dorg.gradle.jvmargs='-Xmx256m -XX:MaxPermSize=64m'" ./gradlew build \
+  --no-daemon \
+  -x verifyGoogleJavaFormat \
+  -x bootDistTar \
+  -x bootDistZip \
+  -x distTar \
+  -x distZip \
+  -x startScripts
 
 mv build/libs/*.jar rest-api.jar
