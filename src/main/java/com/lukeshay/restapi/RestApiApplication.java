0c151da4fe057c64dbf354ec3f50e2b674af7b01
luke.shay <luke.shay@vertexvis.com>
2020-03-26 13:13:43 -0500

    More changes.

--- src/main/java/com/lukeshay/restapi/RestApiApplication.java
+++ src/main/java/com/lukeshay/restapi/RestApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan({"com.lukeshay.restapi"})
 public class RestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
